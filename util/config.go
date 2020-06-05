6bcdb29f3dbc8167284bb85026e24767ee4cc15c
zh 方 <1208244109@qq.com>
2020-06-05 09:34:58 +0800

    add code

--- util/config.go
+++ util/config.go
@@ -1,8 +1,8 @@
 package util
 
 const (
-	DevID       int    = 1570392629
-	DevKey      string = "cbd96055fcad35a37073df7e1c50749e"
+	DevID       int    = 0
+	DevKey      string = "****"
 	SERVER_HOST string = "https://commit-openic.sf-express.com"
-	ShopID      string = "3243279847393"
+	ShopID      string = "***"
 )
